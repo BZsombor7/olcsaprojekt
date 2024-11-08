072ca6652b2a2a8da0c66ecba4a70e70aaf308a6
BZsombor7 <benedekzs.07@gmail.com>
2024-11-08 08:52:58 +0100

    olcsa1

--- szamolo.py
+++ szamolo.py
@@ -1,21 +1,17 @@
 import tkinter as tk
 
-# Számológép alkalmazás osztálya
 class Szamologep:
     def __init__(self, ablak):
         self.ablak = ablak
         self.ablak.title("Számológép")
         self.ablak.geometry("400x600")
         
-        # Eredmény kijelző mező létrehozása
         self.eredmeny_valtozo = tk.StringVar()
         self.eredmeny_cimke = tk.Label(ablak, textvariable=self.eredmeny_valtozo, font=("Arial", 24), anchor="e", bg="white", fg="black")
         self.eredmeny_cimke.grid(row=0, column=0, columnspan=4, sticky="we", ipadx=10, ipady=20)
         
-        # Kezdeti értékek
         self.jelenlegi_bemenet = ""
         
-        # Gombok létrehozása
         gombok = [
             ('7', 1, 0), ('8', 1, 1), ('9', 1, 2), ('/', 1, 3),
             ('4', 2, 0), ('5', 2, 1), ('6', 2, 2), ('*', 2, 3),
@@ -26,14 +22,12 @@ class Szamologep:
         for (szoveg, sor, oszlop) in gombok:
             self.gomb_letrehozas(szoveg, sor, oszlop)
     
-    # Gomb létrehozása függvény
     def gomb_letrehozas(self, szoveg, sor, oszlop):
         gomb = tk.Button(self.ablak, text=szoveg, font=("Arial", 18), command=lambda: self.gomb_megnyomas(szoveg))
         gomb.grid(row=sor, column=oszlop, sticky="nsew", ipadx=20, ipady=20)
         self.ablak.grid_rowconfigure(sor, weight=1)
         self.ablak.grid_columnconfigure(oszlop, weight=1)
     
-    # Gomb megnyomás esemény kezelője
     def gomb_megnyomas(self, karakter):
         if karakter.isdigit() or karakter in "+-*/":
             self.jelenlegi_bemenet += karakter
@@ -50,7 +44,6 @@ class Szamologep:
                 self.eredmeny_valtozo.set("Hiba")
                 self.jelenlegi_bemenet = ""
 
-# Tkinter alkalmazás inicializálása
 ablak = tk.Tk()
 alkalmazas = Szamologep(ablak)
 ablak.mainloop()
